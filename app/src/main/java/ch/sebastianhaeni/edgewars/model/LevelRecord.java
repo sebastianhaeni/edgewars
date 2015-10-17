3f0f2c9f337c90d700cc76d10a63d1a31df7ca0a
raeffu <raeffu@raeffu.ch>
2015-10-17 12:28:12 +0200

    finish LevelRecord
    * storing in DB with Sugar OMR now possible

--- app/src/main/java/ch/sebastianhaeni/edgewars/model/LevelRecord.java
+++ app/src/main/java/ch/sebastianhaeni/edgewars/model/LevelRecord.java
@@ -1,11 +1,13 @@
 package ch.sebastianhaeni.edgewars.model;
 
+import com.orm.SugarRecord;
+
 import java.sql.Time;
 
 /**
  * Created by raeffu on 14/10/15.
  */
-public class LevelRecord {
+public class LevelRecord extends SugarRecord<LevelRecord> {
     private int id;
     private String json_file;
     private int score;
