3a0f60150a3039c95dd2d235d6d64ea35e3addae
raeffu <raeffu@raeffu.ch>
2015-11-11 18:37:53 +0100

    display score and time on level detail activity

--- app/src/main/java/ch/sebastianhaeni/edgewars/ui/activities/LevelDetailActivity.java
+++ app/src/main/java/ch/sebastianhaeni/edgewars/ui/activities/LevelDetailActivity.java
@@ -6,18 +6,27 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.View;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
 import ch.sebastianhaeni.edgewars.R;
 import ch.sebastianhaeni.edgewars.databinding.ActivityLevelDetailBinding;
+import ch.sebastianhaeni.edgewars.model.LevelRecord;
 
 public class LevelDetailActivity extends Activity {
 
     public static final String LEVEL_ID = "LEVEL_ID";
-    private int mLevel;
+    private int mLevelNr;
+    private LevelRecord mLevelRecord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mLevel = getIntent().getExtras().getInt(LEVEL_ID);
+        mLevelNr = getIntent().getExtras().getInt(LEVEL_ID);
+
+        findOrCreateLevelRecord();
 
         ActivityLevelDetailBinding binding = DataBindingUtil.inflate(
                 getLayoutInflater(),
@@ -30,7 +39,7 @@ public class LevelDetailActivity extends Activity {
 
     public void startLevel(View view) {
         Intent intent = new Intent(LevelDetailActivity.this, GameActivity.class);
-        intent.putExtra(LEVEL_ID, mLevel);
+        intent.putExtra(LEVEL_ID, mLevelNr);
         startActivity(intent);
     }
 
@@ -38,8 +47,28 @@ public class LevelDetailActivity extends Activity {
         finish();
     }
 
+    private void findOrCreateLevelRecord() {
+        List<LevelRecord> records = LevelRecord.find(LevelRecord.class, "level_nr = ?", Integer.toString(mLevelNr));
+        if (records.size() > 0) {
+            mLevelRecord = records.get(0);
+        }
+        else {
+            LevelRecord record = new LevelRecord(mLevelNr, 0, 0);
+            record.save();
+            mLevelRecord = record;
+        }
+    }
+
     public int getLevel() {
-        return mLevel;
+        return mLevelNr;
+    }
+
+    public int getScore() { return mLevelRecord.getScore(); }
+    public String getTime () {
+        long millis = mLevelRecord.getTime();
+        SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss", new Locale("de", "CH"));
+        df.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return df.format(millis);
     }
 }
 
