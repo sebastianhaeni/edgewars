e752b2ba88f6d27fcaaf3be5b6d246e93560e70d
Steven Cardini <steven@cardini.ch>
2016-01-11 12:21:47 +0100

    implemented Sebastian's recommendations

--- app/src/main/java/ch/sebastianhaeni/edgewars/logic/entities/Camera.java
+++ app/src/main/java/ch/sebastianhaeni/edgewars/logic/entities/Camera.java
@@ -19,6 +19,12 @@ public class Camera extends Entity {
     private float mCameraScreenX;
     private float mCameraScreenY;
 
+    private final float cameraFactor = 2f / 3f;
+    // constant indicate to which percentage rightmost / bottommost node can be to the right / bottom of the display
+    private final float minOffset = 0.4f;
+    // constant indicate to which percentage leftmost / topmost node can be to the right / bottom of the display
+    private final float maxOffset = 0.6f;
+
     /**
      * Constructor
      */
@@ -49,8 +55,8 @@ public class Camera extends Entity {
      * @param dy delta y
      */
     public void moveCamera(float dx, float dy) {
-        dx = (boundaryIsReached('x', dx)) ? 0 : dx;
-        dy = (boundaryIsReached('y', dy)) ? 0 : dy;
+        dx = (xBoundaryIsReached(dx)) ? 0 : dx;
+        dy = (yBoundaryIsReached(dy)) ? 0 : dy;
 
         mCameraScreenX += dx;
         mCameraScreenY += dy;
@@ -106,29 +112,38 @@ public class Camera extends Entity {
         return mCameraScreenY;
     }
 
-    private boolean boundaryIsReached(char xy, float delta) {
+    /**
+     * Checks whether camera can be moved further along the X axis or if the boundary is reached
+     *
+     * @param delta the amount of pixels the camera shall be moved along the X axis
+     * @return true if boundary on the X axis is reached, false if not
+     */
+    private boolean xBoundaryIsReached(float delta) {
+        Board board = Game.getInstance().getGameState().getBoard();
+        GameRenderer renderer = Game.getInstance().getGameController().getRenderer();
+
+        float rightMostNodeX = renderer.getAndroidCoordinateX(board.getOuterNode(Board.RIGHT).getPosition().getX());
+        rightMostNodeX += mCameraScreenX * cameraFactor;
+        float leftMostNodeX = renderer.getAndroidCoordinateX(board.getOuterNode(Board.LEFT).getPosition().getX());
+        leftMostNodeX += mCameraScreenX * cameraFactor;
+
+        return leftMostNodeX + delta > (renderer.getMaxScreenX() * maxOffset) || rightMostNodeX + delta < (renderer.getMaxScreenX() * minOffset);
+    }
+
+    /**
+     * Checks whether camera can be moved further along the Y axis or if the boundary is reached
+     *
+     * @param delta the amount of pixels the camera shall be moved along the Y axis
+     * @return true if boundary on the Y axis is reached, false if not
+     */
+    private boolean yBoundaryIsReached(float delta) {
         Board board = Game.getInstance().getGameState().getBoard();
         GameRenderer renderer = Game.getInstance().getGameController().getRenderer();
 
         float topMostNodeY = renderer.getAndroidCoordinateY(board.getOuterNode(Board.TOP).getPosition().getY());
+        topMostNodeY += mCameraScreenY * cameraFactor;
         float bottomMostNodeY = renderer.getAndroidCoordinateY(board.getOuterNode(Board.BOTTOM).getPosition().getY());
-        float rightMostNodeX = renderer.getAndroidCoordinateX(board.getOuterNode(Board.RIGHT).getPosition().getX());
-        float leftMostNodeX = renderer.getAndroidCoordinateX(board.getOuterNode(Board.LEFT).getPosition().getX());
-
-        topMostNodeY += mCameraScreenY * (2f / 3f);
-        bottomMostNodeY += mCameraScreenY * (2f / 3f);
-        rightMostNodeX += mCameraScreenX * (2f / 3f);
-        leftMostNodeX += mCameraScreenX * (2f / 3f);
-
-        switch (xy) {
-            case 'x':
-                return leftMostNodeX + delta > (renderer.getMaxScreenX() * .6f) || rightMostNodeX + delta < (renderer.getMaxScreenX() * .4f);
-
-            case 'y':
-                return bottomMostNodeY + delta < (renderer.getMaxScreenY() * .4f) || topMostNodeY + delta > (renderer.getMaxScreenY() * .6f);
-
-            default:
-                throw new IllegalArgumentException("I do not know this coordinate!");
-        }
+        bottomMostNodeY += mCameraScreenY * cameraFactor;
+        return bottomMostNodeY + delta < (renderer.getMaxScreenY() * minOffset) || topMostNodeY + delta > (renderer.getMaxScreenY() * maxOffset);
     }
 }
