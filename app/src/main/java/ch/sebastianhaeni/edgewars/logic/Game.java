ed5fdfe97478a19c6b3b6426e75b0c1074e6a52a
Steven Cardini <steven@cardini.ch>
2016-01-23 13:24:57 +0100

    probably fixed multiple units bug

--- app/src/main/java/ch/sebastianhaeni/edgewars/logic/Game.java
+++ app/src/main/java/ch/sebastianhaeni/edgewars/logic/Game.java
@@ -11,6 +11,7 @@ import ch.sebastianhaeni.edgewars.graphics.GameSurfaceView;
 import ch.sebastianhaeni.edgewars.graphics.drawables.Drawable;
 import ch.sebastianhaeni.edgewars.graphics.drawables.RenderQueue;
 import ch.sebastianhaeni.edgewars.logic.commands.Command;
+import ch.sebastianhaeni.edgewars.logic.commands.MoveUnitCommand;
 import ch.sebastianhaeni.edgewars.logic.entities.Entity;
 import ch.sebastianhaeni.edgewars.logic.entities.Player;
 import ch.sebastianhaeni.edgewars.logic.entities.board.Edge;
@@ -162,12 +163,32 @@ public class Game {
     public void update(long millis) {
 
         // Executing commands, but not more than 5 per cycle
-        int commandCount = mCommandStack.size();
-        while (mCommandStack.size() > 0 && mCommandStack.size() - commandCount < 5) {
+        int commandCount = 0;
+
+        // Stores the Nodes that have sent units already during this cycle
+        ArrayList<Node> moveUnitNodes = new ArrayList<>();
+
+        while (mCommandStack.size() > 0 && commandCount < 5) {
+
             // do not continue execution of commands if game has stopped meanwhile
             if (mGameOver) return;
 
+            // test if it is a MoveUnitCommand and if the node has already sent units in this cycle
+            Command command = mCommandStack.peek();
+            if (command instanceof MoveUnitCommand) {
+                MoveUnitCommand moveCommand = (MoveUnitCommand) command;
+                Node sourceNode = moveCommand.getSourceNode();
+                if (moveUnitNodes.contains(sourceNode)) {
+                    // skip this command, since the node has sent units already
+                    mCommandStack.pop();
+                    continue;
+                } else {
+                    moveUnitNodes.add(sourceNode);
+                }
+            }
+
             mCommandStack.pop().execute();
+            commandCount++;
         }
 
         for (Map.Entry<Entity, Long> pair : mEntities.entrySet()) {
@@ -211,7 +232,7 @@ public class Game {
                 break;
             }
         }
-        if(gameOver) {
+        if (gameOver) {
             won = owner.isHuman();
         }
         result[0] = gameOver;
@@ -270,6 +291,24 @@ public class Game {
         }
 
         return neighbors;
+    }
+
+    /**
+     * Gets the Node that is connected to the specified Node via specified Edge.
+     *
+     * @param node the node
+     * @param edge the edge
+     * @return the opposite node or <code>null</code>
+     */
+    public Node getOppositeNode(Node node, Edge edge) {
+        List<Node> neighborNodes = this.getConnectedNodes(node);
+        for (Node currentNode : neighborNodes) {
+            Edge currentEdge = this.getEdgeBetween(node, currentNode);
+            if (edge.equals(currentEdge)) {
+                return currentNode;
+            }
+        }
+        throw new RuntimeException("This edge is not connected to the node!");
     }
 
     /**
